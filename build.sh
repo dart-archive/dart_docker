78355cff56d0435eecba3950aef88af2cea6a5f2
Søren Gjesse <sgjesse@google.com>
2014-08-28 14:45:34 +0200

    Update Dockerfile to Dart 1.6.0
    R=kustermann@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org//514153002

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #! /bin/bash
 # Build the Docker images for Dart.
-VERSION=1.5.8
-docker build -t google/dart base
-docker tag google/dart google/dart:$VERSION
-docker build -t google/dart-runtime runtime
-docker tag google/dart google/dart-runtime:$VERSION
+#
+# This is only used to test the Docker images locally. For distribution
+# the automated builds on hub.docker.com is used.
+VERSION=1.6.0
+REPOSITORY_PREFIX=google-test
+docker build -t $REPOSITORY_PREFIX/dart base
+docker tag $REPOSITORY_PREFIX/dart $REPOSITORY_PREFIX/dart:$VERSION
+docker build -t $REPOSITORY_PREFIX/dart-runtime runtime
+docker tag $REPOSITORY_PREFIX/dart $REPOSITORY_PREFIX/dart-runtime:$VERSION
 cd hello
 pub update
 cd ..
-docker build -t google/dart-hello hello
-docker tag google/dart-hello google/dart-hello:$VERSION
+docker build -t $REPOSITORY_PREFIX/dart-hello hello
+docker tag $REPOSITORY_PREFIX/dart-hello $REPOSITORY_PREFIX/dart-hello:$VERSION
