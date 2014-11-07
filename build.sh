4d11075bcd7268f12c0692a33f8fa0af81f9bbfd
Søren Gjesse <sgjesse@google.com>
2014-11-07 13:37:44 +0100

    Setup for building with Dart version 1.8.0-dev.2.0

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 #
 # This is only used to test the Docker images locally. For distribution
 # the automated builds on hub.docker.com is used.
-VERSION=1.7.2
+VERSION=1.8.0-dev.2.0
 REPOSITORY_PREFIX=google_test
 echo 'Building base'
 docker build -t $REPOSITORY_PREFIX/dart base
