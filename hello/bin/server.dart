760b7438aec2447e21464e02f4257fe998524a5d
Brett Morgan <brett.morgan@gmail.com>
2019-03-28 00:00:03 +1100

    Adding PORT environment variable (#37)
    * Adding PORT environment variable
    
    Fix for https://github.com/dart-lang/dart_docker/issues/31
    
    * Cleaning up

--- hello/bin/server.dart
+++ hello/bin/server.dart
@@ -7,28 +7,28 @@ import 'dart:io';
 import 'package:http_server/http_server.dart';
 
 void main() {
-  var webFiles = new VirtualDirectory('web');
+  final port = int.tryParse(Platform.environment['PORT'] ?? '8080');
+  final webFiles = new VirtualDirectory('web');
 
   runZoned(() {
-    HttpServer.bind('0.0.0.0', 8080).then((server) {
+    HttpServer.bind('0.0.0.0', port).then((server) {
       server.listen((request) {
-          if (request.uri.path == '/') {
-            request.response.redirect(request.uri.resolve('/index.html'));
-          } else if (request.uri.path == '/version') {
-            request.response.headers..contentType = ContentType.TEXT;
-            request.response
-                ..writeln('Dart version: ${Platform.version}')
-                ..writeln('Dart executable: ${Platform.executable}')
-                ..writeln('Dart executable arguments: '
-                          '${Platform.executableArguments}')
-                ..close();
-          } else {
-            webFiles.serveRequest(request);
-          }
+        if (request.uri.path == '/') {
+          request.response.redirect(request.uri.resolve('/index.html'));
+        } else if (request.uri.path == '/version') {
+          request.response.headers..contentType = ContentType.text;
+          request.response
+            ..writeln('Dart version: ${Platform.version}')
+            ..writeln('Dart executable: ${Platform.executable}')
+            ..writeln('Dart executable arguments: '
+                '${Platform.executableArguments}')
+            ..close();
+        } else {
+          webFiles.serveRequest(request);
+        }
       });
     });
-  },
-  onError: (e, stackTrace) {
+  }, onError: (e, stackTrace) {
     print('Error processing request $e\n$stackTrace');
   });
 }
